f93cbb3da767c03ac605008cca1ab1ad0d8bdd10
dalmatian <stitt4@llnl.gov>
2018-05-17 16:52:13 -0700

    cleanup verify_mpi update setup.py to build parcop.so python module using 'current' f2py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+################################################################################
+# Copyright (c) 2018, Lawrence Livemore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# LLNL-CODE-749864
+# This file is part of pyranda
+# For details about use and distribution, please read: pyranda/LICENSE
+################################################################################
 import os
 import sys
 import subprocess
@@ -11,6 +19,25 @@ try:
 except ImportError as e:
     print(e)
     sys.exit("for now we require you to pre-install mpi4py and numpy")
+
+# try to get the mpif90 compiler from mpi4py, it isn't always there..
+# if a user installs like `env MPICC=/path/to/mpicc pip install mpi4py ...`
+# it doesn't seem to have anything other than mpicc
+mpi4py_compilers = mpi4py.get_config()
+if 'mpif90' in mpi4py_compilers:
+    default_mpif90 = mpi4py_compilers['mpif90']
+elif 'mpifort' in mpi4py_compilers:
+    default_mpif90 = mpi4py_compilers['mpifort']
+# last effort, try to build the possible location
+elif 'mpicc' in mpi4py_compilers and os.path.exists(mpi4py_compilers['mpicc'][:-2] + 'f90'):
+    default_mpif90 = mpi4py_compilers['mpicc'][:-2] + 'f90'
+else:
+    default_mpif90 = 'mpif90'
+
+# defaults for options used when launching the verify mpi script
+default_mpiexec = 'mpirun'
+default_mpiexec_nprocs_arg = '-n'
+default_mpiexec_nprocs = 4
 
 distname = "pyranda"
 fortran_module = 'parcop'
@@ -28,53 +55,79 @@ hyperbolic PDE systems. This is the mini-app for the Miranda code.
 
 class PyrandaMakeMixin():
     user_options = [
-        ('mpif90=', None, 'mpif90 compiler'),
-        ('mpiexec=', None, 'mpi launcher command'),
-        ('numprocs-arg=', None, 'mpi launcher nprocs arg')
+        ('mpif90=', None, 'mpif90 compiler (default: {})'.format(default_mpif90)),
+        ('mpiexec=', None, 'mpi exec command used to verify when verifying the mpi compiler (default: {})'.format(default_mpiexec)),
+        ('numprocs-arg=', None, 'mpi exec num procs arg used when verifying the mpi compiler (default: {})'.format(default_mpiexec_nprocs_arg)),
+        ('numprocs=', None, 'number of procs used when verifying mpi (default: {})'.format(default_mpiexec_nprocs)),
+        ('no-mpi-compiler-check=', None, 'disable checking that the mpi compiler is compatible with mpi4py')
     ]
 
     def initialize_options(self):
         self.mpif90 = None
         self.mpiexec = None
         self.numprocs_arg = None
+        self.numprocs = None
+        self.no_mpi_compiler_check = None
 
     def finalize_options(self):
         if self.mpif90 is None:
-            self.mpif90 = 'mpif90'
+            self.mpif90 = default_mpif90
         if self.mpiexec is None:
-            self.mpiexec = 'mpirun'
+            self.mpiexec = default_mpiexec
         if self.numprocs_arg is None:
-            self.numprocs_arg = '-n'
+            self.numprocs_arg = default_mpiexec_nprocs_arg
+        if self.numprocs is None:
+            self.numprocs = default_mpiexec_nprocs
+        if self.no_mpi_compiler_check is None:
+            self.no_mpi_compiler_check = False
+
+    def clean(self):
+        print("cleaning up from {} build".format(fortran_module_target))
+        try:
+            subprocess.check_call(['make', '-C', fortran_package, 'clean'])
+        except subprocess.CalledProcessError:
+            print("failed to clean {}".format(fortran_module_target))
+            raise
+        print("{} cleaned".format(fortran_module_target))
 
     def run(self):
         python = sys.executable
+        # verify that the mpif90 provided works
+        if not self.no_mpi_compiler_check:
+            print("verifying that mpi4py is compatible with {}".format(self.mpif90))
+            try:
+                subprocess.check_call([self.mpiexec, self.numprocs_arg, '4', python, 'verify_mpi.py', self.mpif90])
+            except subprocess.CalledProcessError:
+                print("failed to verify mpi")
+                raise
+            except KeyboardInterrupt:
+                pass
+            print("{} is compatible".format(self.mpif90))
 
-        print("verifying that mpi4py is compatiable with your mpifortran compiler")
-        try:
-            subprocess.check_call([self.mpiexec, self.numprocs_arg, '4', python, 'verify_mpi.py', self.mpif90])
-        except subprocess.CalledProcessError:
-            print("failed to verify mpi")
-            raise
-        print("{} is compatible".format(self.mpif90))
-
+        # build lib*.a
         print("building {}".format(fortran_module_target))
+        f2py_cmd = "{} -c 'import numpy.f2py as f2py; f2py.main()'".format(python)
         try:
-            subprocess.check_call(['make', '-C', fortran_package, 'f90={}'.format(self.mpif90), 'f2py=f2py'])#fortran_module_target])
+            #subprocess.check_call(['make', '-C', fortran_package, 'f90={}'.format(self.mpif90), fortran_module_target])
+            subprocess.check_call(['make', '-C', fortran_package, 'f90={}'.format(self.mpif90), 'f2py={}'.format(f2py_cmd)])
         except subprocess.CalledProcessError:
             print("failed to build {}".format(fortran_module_target))
             raise
-    
-        '''
-        print("building parcop's python interface")
-        with open(os.path.join(fortran_package, fortran_module_interface), 'r') as _:
-            source = _.read()
-        status = f2py.compile(source,
-            extension='.f90',
-            extra_args='--f90exec={} {} --include-paths ./pyranda/parcop'.format(self.mpif90, fortran_module_target),
-            modulename=fortran_module)
-        if status != 0:
-            raise RuntimeError('failed to compile the fortran python interface')
-        '''
+        print("{} built".format(fortran_module_target))
+
+        # we could possible build this as an extension:
+        # https://docs.scipy.org/doc/numpy-1.14.0/f2py/distutils.html
+        # but at this time (5/17/18) numpy.distutils.core.setup doesn't
+        # support install_requires
+        #print("building {}'s python interface".format(fortran_module))
+        #with open(os.path.join(fortran_package, fortran_module_interface), 'r') as _:
+        #    source = _.read()
+        #status = f2py.compile(source,
+        #    extension='.f90',
+        #    extra_args='--f90exec={} ...'.format(self.mpif90),
+        #    modulename=fortran_module)
+        #if status != 0:
+        #    raise RuntimeError('failed to compile the fortran python interface')
 
 
 class BuildPyranda(build, PyrandaMakeMixin):
@@ -89,24 +142,21 @@ class BuildPyranda(build, PyrandaMakeMixin):
         PyrandaMakeMixin.finalize_options(self)
 
     def run(self):
-        build.run(self)
         PyrandaMakeMixin.run(self)
+        build.run(self)
 
 
 class InstallPyranda(install, PyrandaMakeMixin):
-    user_options = install.user_options + PyrandaMakeMixin.user_options
 
     def initialize_options(self):
         install.initialize_options(self)
-        PyrandaMakeMixin.initialize_options(self)
 
     def finalize_options(self):
         install.finalize_options(self)
-        PyrandaMakeMixin.finalize_options(self)
 
     def run(self):
         install.run(self)
-        PyrandaMakeMixin.run(self)
+        PyrandaMakeMixin.clean(self)
 
 
 setup_args = dict(
--- verify_mpi.py
+++ verify_mpi.py
@@ -1,86 +1,110 @@
+################################################################################
+# Copyright (c) 2018, Lawrence Livemore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# LLNL-CODE-749864
+# This file is part of pyranda
+# For details about use and distribution, please read: pyranda/LICENSE
+################################################################################
+from mpi4py import MPI
 import sys
 import argparse
-
-# make sure we have f2py
-try:
-    from numpy import f2py
-except ImportError as e:
-    print(e)
-    sys.exit("cannot import f2py")
-
-
-# make sure we can initialize mpi
-try:
-    from mpi4py import MPI
-except ImportError as e:
-    print(e)
-    sys.exit("cannot import mpi4py")
+import glob
+import shutil
+import os
+from numpy import f2py
 
 # init mpi
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+# the fortran function we try to run
 source = b'''
 function sum_comm_ranks(comm) result(ranksum)
   use mpi
   implicit none
   integer, intent(in) :: comm
-  integer :: ranksum, rank, err
-  call MPI_Comm_rank(comm, rank, err)
-  call MPI_Allreduce(rank, ranksum, 1, MPI_INT, MPI_SUM, comm, err)
+  integer :: ranksum, mpirank, err
+  call MPI_Comm_rank(comm, mpirank, err)
+  call MPI_Allreduce(mpirank, ranksum, 1, MPI_INT, MPI_SUM, comm, err)
 end function sum_comm_ranks
 '''
 
-mod_name = 'fort_mod'
+# parse command line
+status = 0
+if rank == 0:
+    parser = argparse.ArgumentParser(description='test mpi4py compatibility')
+    parser.add_argument('mpif90', help='path to mpif90 compiler')
 
-parser = argparse.ArgumentParser(description='test mpi4py compatibility')
-parser.add_argument('mpif90', help='path to mpif90 compiler')
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        status = 1
 
-args = parser.parse_args()
+if comm.bcast(status, root=0) != 0:
+    sys.exit(1)
+
 
 # compile the fort module
 status = 0
+# mod_name is used below in an import
+mod_name = 'fort_mod'
 if rank == 0:
     status = f2py.compile(source,
         extension='.f90',
         extra_args='--f90exec={}'.format(args.mpif90),
+        verbose=False,
         modulename=mod_name)
 
     if status != 0:
         print("compile failed!")
 
-status = comm.bcast(status, root=0)
-
-if status != 0:
+if comm.bcast(status, root=0) != 0:
     sys.exit(1)
+
+
+def print0(arg):
+    if rank == 0:
+        print(arg)
+
+
+def cleanup():
+    if rank == 0:
+        files = glob.glob('{}*'.format(mod_name))
+        for f in files:
+            print("removing {}".format(f))
+            if os.path.isfile(f):
+                os.remove(f)
+            elif os.path.isdir(f):
+                shutil.rmtree(f)
+
 
 # try to import the fortran module
 try:
-    if rank == 0:
-        print("trying to import fortran module")
+    print0("trying to import fortran module...")
     from fort_mod import sum_comm_ranks
 except ImportError as e:
-    print(e)
+    print0(e)
     sys.exit("cannot import fortran module")
+finally:
+    cleanup()
+
+print0("successfully imported")
+
 
 # make sure it functions as expected
-if rank == 0:
-    print("trying to run fortran function")
+print0("trying to run fortran function...")
+# tries to make the output ordering nice
+comm.barrier()
 fsum = sum_comm_ranks(comm.py2f())
 expected = sum(range(comm.Get_size()))
-
+print("rank {}: expected: {}, got: {}".format(rank, fsum, expected))
 if fsum != expected:
-    print("rank {}: {} != {}".format(rank, fsum, expected))
+    print("expected != got")
+else:
+    print0("successfully ran")
 
 assert(fsum == expected)
 
 if rank == 0:
-    import glob, shutil, os
-    files = glob.glob('{}*'.format(mod_name))
-    for f in files:
-        print("removing {}".format(f))
-        if os.path.isfile(f):
-            os.remove(f)
-        elif os.path.isdir(f):
-            shutil.rmtree(f)
     print("all good!")
