f9fa454ae9cbb3726b85422c660149d88904ad63
LeviBarnes <lbarnes@nvidia.com>
2018-07-13 12:11:05 -0700

    Need iso_c_binding in nvtx_stub

--- pyranda/parcop/nvtx_stub.f90
+++ pyranda/parcop/nvtx_stub.f90
@@ -1,5 +1,6 @@
 ! When nvtx is turned off, call these stubs instead 
 module nvtx
+   use iso_c_binding
    contains
    subroutine nvtxStartRange(tag, id)
       character(kind=c_char,len=*) :: tag
