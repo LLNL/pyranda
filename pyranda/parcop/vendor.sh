d699ee2419a7647d8a88125d8076987240555b32
Thomas Stitt <stitt4@llnl.gov>
2018-08-28 14:34:26 -0700

    handle cases where --version is the right arg but returns non-0, put gcc last so it doesn't capture pgcc..

--- pyranda/parcop/vendor.sh
+++ pyranda/parcop/vendor.sh
@@ -3,13 +3,14 @@
 default=default
 
 info=$($1 --version 2>/dev/null)
-# older ibm
-test $? -ne 0 && info=$($1 -qversion 2>/dev/null) || true
+# += because certain compilers return non-0 status with --version
+test $? -ne 0 && info+=$($1 -qversion 2>/dev/null) || true
 test $? -ne 0 && { echo "unknown compiler"; exit 0; } || true
 
-echo $info | grep -iE "gnu|gcc" > /dev/null && { echo "gnu"; exit 0; }
-echo $info | grep -i "clang" > /dev/null && { echo "clang"; exit 0; }
 echo $info | grep -i "intel" > /dev/null && { echo "intel"; exit 0; }
 echo $info | grep -i "ibm" > /dev/null && { echo "ibm"; exit 0; }
+echo $info | grep -i "pgi" > /dev/null && { echo "pgi"; exit 0; }
+echo $info | grep -i "clang" > /dev/null && { echo "clang"; exit 0; }
+echo $info | grep -iE "gnu|gcc" > /dev/null && { echo "gnu"; exit 0; }
 
 echo $default
