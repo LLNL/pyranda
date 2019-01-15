73f356c40c11ec38b60d0bfe7925150094fdc966
flow-phys <britton.olson@gmail.com>
2019-01-15 14:02:48 -0800

    Extrapolation to 2nd order and part of free-slip BCs

--- pyranda/pyrandaBC.py
+++ pyranda/pyrandaBC.py
@@ -56,7 +56,7 @@ class pyrandaBC(pyrandaPackage):
                     pFunc( v, d  )
         
         
-    def extrap(self,var,direction):
+    def extrap(self,var,direction,order=1):
 
         if type(var) != type([]):
             var = [var]
@@ -66,30 +66,40 @@ class pyrandaBC(pyrandaPackage):
 
         for d in direction:
             for v in var:
-                self.extrapolate( v , d )
+                self.extrapolate( v , d ,order)
             
         
 
-    def extrapolate(self,var,direction):
+    def extrapolate(self,var,direction,order):
         # Direction switch
         bcvar = None
         if direction == 'x1':
             if self.pyranda.PyMPI.x1proc:
-                self.pyranda.variables[var].data[0,:,:] = 2*self.pyranda.variables[var].data[1,:,:] - self.pyranda.variables[var].data[2,:,:] 
-                #self.pyranda.variables[var].data[0,:,:] = self.pyranda.variables[var].data[1,:,:] 
+                if order == 2:
+                    self.pyranda.variables[var].data[0,:,:] = 2*self.pyranda.variables[var].data[1,:,:] - self.pyranda.variables[var].data[2,:,:] 
+                else:
+                    self.pyranda.variables[var].data[0,:,:] = self.pyranda.variables[var].data[1,:,:] 
 
         if direction == 'xn':
             if self.pyranda.PyMPI.xnproc:
-                self.pyranda.variables[var].data[-1,:,:] = 2*self.pyranda.variables[var].data[-2,:,:] - self.pyranda.variables[var].data[-3,:,:]
-                #self.pyranda.variables[var].data[-1,:,:] = self.pyranda.variables[var].data[-2,:,:]
+                if order == 2:
+                    self.pyranda.variables[var].data[-1,:,:] = 2*self.pyranda.variables[var].data[-2,:,:] - self.pyranda.variables[var].data[-3,:,:]
+                else:
+                    self.pyranda.variables[var].data[-1,:,:] = self.pyranda.variables[var].data[-2,:,:]
 
         if direction == 'y1':
             if self.pyranda.PyMPI.y1proc:
-                self.pyranda.variables[var].data[:,0,:] = self.pyranda.variables[var].data[:,1,:]
+                if order == 2:
+                    self.pyranda.variables[var].data[:,0,:] = 2*self.pyranda.variables[var].data[:,1,:] - self.pyranda.variables[var].data[:,2,:]
+                else:
+                    self.pyranda.variables[var].data[:,0,:] = self.pyranda.variables[var].data[:,1,:]
 
         if direction == 'yn':
             if self.pyranda.PyMPI.ynproc:
-                self.pyranda.variables[var].data[:,-1,:] = self.pyranda.variables[var].data[:,-2,:]
+                if order == 2:
+                    self.pyranda.variables[var].data[:,-1,:] = 2*self.pyranda.variables[var].data[:,-2,:] - self.pyranda.variables[var].data[:,-3,:]
+                else:
+                    self.pyranda.variables[var].data[:,-1,:] = self.pyranda.variables[var].data[:,-2,:]
 
                 
     def const(self,var,direction,val):
@@ -241,6 +251,11 @@ class pyrandaBC(pyrandaPackage):
             else:
                 norms = self.BCdata['slipbc-y1']
 
+                
+            # For free slip, always extrapolate first
+            for uu in velocity:
+                self.extrapolate(uu,'y1',order=2)
+                
             if self.pyranda.PyMPI.y1proc:
 
                 udotn = 0.0
