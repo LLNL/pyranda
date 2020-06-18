bb6e5b2df85491249f724b46802299d98a74bc87
Aaron Larsen <aaronml@byu.edu>
2020-06-18 14:44:28 -0600

    Fixes RK4_step

--- pyranda/pyranda.py
+++ pyranda/pyranda.py
@@ -196,7 +196,7 @@ class pyrandaSim:
         Higher level wrapper to make equations of motion from a single string
         Will add eqautions and variables as needed.
         """
-
+        print(eomDict)
         # Apply a dictionary to the string
         if eomDict:
             for eomd in eomDict:
@@ -210,6 +210,7 @@ class pyrandaSim:
         
         # Split up the equation lines
         eom_lines = filter(None,eom.split('\n'))
+        print(eom_lines)
         eom_lines = [el for el in eom_lines if el.strip()[0] != '#']  # Comments work
         var_names = []
 
@@ -847,12 +848,14 @@ class pyrandaSim:
         tmp1 = {}
         tmp2 = {}
 
+        if not PHI:
+            PHI  = {}
+            for U in self.conserved: 
+                PHI[U]  = numpy.asfortranarray( numpy.zeros(  shape ) )
+        
         for U in self.conserved: 
             tmp1[U] = numpy.asfortranarray( numpy.zeros(  shape ) )
             tmp2[U] = numpy.asfortranarray( numpy.zeros(  shape ) )
-            if not PHI:
-                PHI  = {}
-                PHI[U]  = numpy.asfortranarray( numpy.zeros(  shape ) )
         
         # Get primative flow variables
 
