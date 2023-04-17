c75c36f13021f9e8818c3852f6fce579821350f7
Britton Olson <olson45@llnl.gov>
2023-04-17 11:25:54 -0700

    Adding new baselines for spherical (1D/2D).

--- tests/cases/test2deuler.py
+++ tests/cases/test2deuler.py
@@ -3,6 +3,8 @@ baselines = """
 euler-2d-64 -- euler-2d-64.dat
 euler-2d-128 -- euler-2d-128.dat
 euler-RZ-128 -- euler-RZ-128.dat
+euler-SPH-1D -- euler-SPH-1D.dat
+euler-SPH-2D -- euler-SPH-2D.dat
 KH-2d-64 -- KelvinHelmholtzKH-2d-64.dat
 """
 
@@ -23,6 +25,18 @@ test = testObj('euler-RZ-128')
 test.script = 'examples/eulerRZ.py'
 test.args = ['128','1',test.name]
 tests.append( test )
+
+test = testObj('euler-SPH-1D')
+test.script = 'examples/eulerSpherical1D.py'
+test.args = ['128','1',test.name]
+tests.append( test )
+
+test = testObj('euler-SPH-2D')
+test.script = 'examples/eulerSpherical.py'
+test.args = ['32','1',test.name]
+tests.append( test )
+
+
 
 # Kelvin-Helmholtz test problem
 test = testObj('KH-2d-64')
