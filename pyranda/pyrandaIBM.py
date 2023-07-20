01f2549ae5854227e1d1909b9ef5b342b86c42b5
Bryn Barker <barker38@syrah256.llnl.gov>
2023-07-20 14:26:54 -0700

    add wall model

--- pyranda/pyrandaIBM.py
+++ pyranda/pyrandaIBM.py
@@ -29,8 +29,116 @@ class pyrandaIBM(pyrandaPackage):
         sMap['ibmV('] = "self.packages['IBM'].ibmVel("
         sMap['ibmS('] = "self.packages['IBM'].ibmS("
         self.sMap = sMap
+
         
                  
+   def ibmWallModel(self,vel,phi,gphi,global_u0,phivar=None):
+       # do we have dx == dy == dz ????
+       dx = self.pyranda.mesh.dx
+       lo = self.pyranda.PyMPI.chunk_3d_lo
+       ax, ay, az = phi.shape
+       u0 = numpy.zeros((ax, ay, az))
+
+       phix = gphi[0]
+       phiy = gphi[1]
+       phiz = gphi[2]
+
+       tmp0 = (h_wm-phi)*phix
+       tmp1 = (h_wm-phi)*phiy
+       tmp2 = (h_wm-phi)*phiz
+
+       shift0 = (tmp0/dx).astype(int)
+       shift1 = (tmp1/dy).astype(int)
+       shift2 = (tmp2/dz).astype(int)
+
+       offset0 = (tmp0 - dx*shift0)/dx
+       offset1 = (tmp1 - dy*shift1)/dy
+       offset2 = (tmp2 - dz*shift2)/dz
+
+       def interp(C):
+           c00 = C[0,0,0]*(1-offset0)+C[1,0,0]*offset0
+           c01 = C[0,0,1]*(1-offset0)+C[1,0,1]*offset0
+           c10 = C[0,1,0]*(1-offset0)+C[1,1,0]*offset0
+           c11 = C[0,1,1]*(1-offset0)+C[1,1,1]*offset0
+
+           c0 = c00*(1-offset1)+c10*offset1
+           c1 = c01*(1-offset1)+c11*offset1
+
+           c = c0*(1-offset2)+c1*offset2
+           return c
+
+       for i in range(ax):
+           for j in range(ay):
+               for k in range(az):
+                   ii = lo[0]+i+shift0
+                   jj = lo[1]+j+shift1
+                   kk = lo[2]+k+shift2
+                   neighbs = global_u0[ii:ii+2,jj:jj+2,kk:kk+2]
+                   u0[i,j,k] = interp(neighbs)
+
+       # implement slip velocity and enforce wall model bc
+       # essentially copying the slip velocity function with slip=True
+       v1 = vel[0]*1.0
+       v2 = vel[1]*1.0
+       v3 = vel[2]*1.0
+
+       if phivar:
+           v1_phi = phivar[0]
+           v2_phi = phivar[1]
+           v3_phi = phivar[2]
+
+           # Transform to interface velocity
+           v1 -= v1_phi
+           v2 -= v2_phi
+           v3 -= v3_phi
+
+       v1 = self.smooth_terrain(SDF,gDx,gDy,gDz,v1,0.0,new=new)
+       v2 = self.smooth_terrain(SDF,gDx,gDy,gDz,v2,0.0,new=new)
+       v3 = self.smooth_terrain(SDF,gDx,gDy,gDz,v3,0.0,new=new)
+
+       normal = v1*gDx+v2*gDy+v3*gDz
+
+       vn =  numpy.where( SDF < lens, normal, 0.0 )
+
+       # Remove normal velocity
+       v1 = v1 - vn*gDx
+       v2 = v2 - vn*gDy
+       v3 = v3 - vn*gDz
+
+       # Normalize the tangent vector
+       norm = numpy.sqrt( v1*v1+v2*v2+v3*v3 )
+       u1 = v1/norm
+       u2 = v2/norm
+       u3 = v3/norm
+
+       # set velocity to zero for phi < 0
+       interior_mask = numpy.where( SDF < lens, 0.0, 1.0)
+       v1 = v1*interior_mask
+       v2 = v2*interior_mask
+       v3 = v3*interior_mask
+
+       # Compute linear velocity through zero level
+       tmp = numpy.where( SDF < lens, 0.0 , normal/(SDF) )
+       tmp_n = self.smooth_terrain(SDF,gDx,gDy,gDz,tmp,lens,new=False)
+       vn = numpy.where( SDF < lens, tmp_n*SDF, )
+
+       # Compute the linear velocity through the wall model bc
+       tmp = numpy.where( SDF < lens, u0 , normal/(SDF) )
+       tmp_t = self.smooth_terrain(SDF,gDx,gDy,gDz,tmp,lens,new=False)
+       vt = numpy.where( SDF < lens, tmp_t*SDF, norm )
+
+       # Add velocity linear profile in tangential and normal directions
+       v1 = v1 + vt*u1 + vn*gDx
+       v2 = v2 + vt*u2 + vn*gDy
+       v3 = v3 + vt*u3 + vn*gDz
+
+       if phivar:
+           v1 += v1_phi
+           v2 += v2_phi
+           v3 += v3_phi
+
+       return [v1, v2, v3]
+
     def ibmVel(self,vel,phi,gphi,phivar=None):
 
         u = vel[0]
